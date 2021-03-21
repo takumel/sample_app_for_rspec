c6848e16d4829d7c5a146a00f840001ee98d2dd4
taku horikoshi <taku@takus-mac.local>
2021-03-22 03:18:56 +0900

    add user system spec

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -6,29 +6,29 @@ RSpec.describe 'UserSessions', type: :system do
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する'
-      visit login_ptah
-      fill_in "Email",	with: "user.email" 
-      fill_in "Password",	with: "hoge"
-      expect(page).to have_content 'Login successful'
-      expect(current_paht).to eq root_path   
+        visit login_ptah
+        fill_in "Email",	with: "user.email" 
+        fill_in "Password",	with: "password"
+        expect(page).to have_content 'Login successful'
+        expect(current_paht).to eq root_path   
     end
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する'
-      visit login_path
-      fill_in "Email",	with: "" 
-      fill_in "Password",	with: "hoge"
-      expect(page).to have_content 'Login failed'
-      expect(current_paht).to eq login_path
+        visit login_path
+        fill_in "Email",	with: "" 
+        fill_in "Password",	with: "password"
+        expect(page).to have_content 'Login failed'
+        expect(current_paht).to eq login_path
     end
   end
 
   describe 'ログイン後' do
     context 'ログアウトボタンをクリック' do
       it 'ログアウト処理が成功する'
-      login_as(user)
-      click_link 'Logout'
-      expect(page).to have_content 'Loged out'
-      expect(current_path).to eq root_path 
+        login_as(user)
+        click_link 'Logout'
+        expect(page).to have_content 'Loged out'
+        expect(current_path).to eq root_path 
     end
   end
 end
